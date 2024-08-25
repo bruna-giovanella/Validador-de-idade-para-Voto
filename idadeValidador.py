828ee1246562634981e429af055d45902b1982fc
Bru <166079160+bruna-giovanella@users.noreply.github.com>
2024-08-25 17:57:32 -0300

    Update idadeValidador.py

--- idadeValidador.py
+++ idadeValidador.py
@@ -1,37 +1,38 @@
-#Verificador se a idade do usuário se encaixa como a de um eleitor
+
+#Verificador se a age do usuário se encaixa como a de um eleitor
 
 from datetime import date
 
-def validadorData(data_nascimento):
+def validatorData(birth_date):
     c = 0
     c1 = 0
-    for i in data_nascimento:
+    for i in birth_date:
         if i in ('1', '2', '3', '4', '5', '6', '7', '8', '9', '0'):
             c += 1
         if i in '/':
             c1 += 1
-    if (c == 8 and len(data_nascimento) == 10 and c1 == 2) or (c == 7 and len(data_nascimento) == 9 and c1 == 2) or (c == 6 and len(data_nascimento) == 8 and c1 == 2):
-        nascimento(data_nascimento)
+    if (c == 8 and len(birth_date) == 10 and c1 == 2) or (c == 7 and len(birth_date) == 9 and c1 == 2) or (c == 6 and len(birth_date) == 8 and c1 == 2):
+        birth(birth_date)
     else:
         print('Data informada é inválida\nInforme sua data de nascimento no formato dd/mm/aaaa')
-        data_nascimento = input('Nova data: ')
-        validadorData(data_nascimento)
+        birth_date = input('Nova data: ')
+        validatorData(birth_date)
 
 
-def nascimento(data_nascimento):
-    data_nascimento = data_nascimento.split('/')
-    ano_nascimento = [year for year in data_nascimento if len(year) == 4]
-    ano_nascimento = int(ano_nascimento[0])
-    idade(ano_nascimento)
+def birth(birth_date):
+    birth_date = birth_date.split('/')
+    birth_year = [year for year in birth_date if len(year) == 4]
+    birth_year = int(birth_year[0])
+    age(birth_year)
 
 
-def idade(ano_nascimento):
-    ano = int(date.today().year)
-    idade = ano - ano_nascimento
-    
-    if idade >= 18 and idade <= 70:
+def age(birth_year):
+    year = int(date.today().year)
+    age = year - birth_year
+
+    if age >= 18 and age <= 70:
         print('Você está apto a participar das eleições!\nSeu voto é obrigatório.')
-    elif idade >= 16 or idade > 70:
+    elif age >= 16 or age > 70:
         print('Você está apto a participar das eleições!\nMas seu voto não é obrigatório.')
     else:
         print('Você não está apto a participar das eleições.')
@@ -39,7 +40,7 @@ def idade(ano_nascimento):
 
 
 print('Informe sua data de nascimento no formato dd/mm/aaaa')
-data_nascimento = input('data: ')
+birth_date = input('data: ')
 
-validadorData(data_nascimento)
+validatorData(birth_date)
 
